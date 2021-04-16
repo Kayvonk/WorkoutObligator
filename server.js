4dc604bca849b0cf3ebda34f7a0a8d823de491ad
kayvonk <kayvonk@gmail.com>
2021-04-15 18:07:26 -0600

    Added route for stats page

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://kayvonk:mongo-rising88@cluster0.p20ew.mongodb.net/mainDatabase?authSource=admin&replicaSet=atlas-71zs61-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
